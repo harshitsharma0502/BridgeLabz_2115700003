6992d4dcf045494a406ab012f571dda920e0a904
Harshit <harshitsharma0502@gmail.com>
2025-01-23 15:06:40 +0530

    sixth Problem Solution uplaoded

---
+++ Problem_6.cs
@@ -0,0 +1,22 @@
+using System;
+
+class NumberCheck
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        if (number > 0)
+        {
+            Console.WriteLine("The number is positive.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("The number is negative.");
+        }
+        else
+        {
+            Console.WriteLine("The number is zero.");
+        }
+    }
+}
