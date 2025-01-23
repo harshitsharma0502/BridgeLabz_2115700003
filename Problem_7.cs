6fad3662eb8d21d5d31c3e5bf0e7ddda7f478eb5
Harshit <harshitsharma0502@gmail.com>
2025-01-23 15:17:55 +0530

    Seventh Problem Solution uplaoded

---
+++ Problem_7.cs
@@ -0,0 +1,21 @@
+using System;
+
+class SpringSeason
+{
+    static void Main(string[] args)
+    {
+        int month = int.Parse(args[0]);
+        int day = int.Parse(args[1]);
+        if ((month == 3 && day >= 20) || 
+            (month == 4) || 
+            (month == 5) || 
+            (month == 6 && day <= 20))
+        {
+            Console.WriteLine("Its a Spring Season");
+        }
+        else
+        {
+            Console.WriteLine("Not a Spring Season");
+        }
+    }
+}
